5dd7c3e2184a56377e46dd614d11228baaa42ac0
Marvin Cespedes <marvin.cespedes@cityofchicago.org>
2014-05-21 15:44:57 -0500

    removing spam fields

--- httpdocs/wp-content/themes/occwp/footer.php
+++ httpdocs/wp-content/themes/occwp/footer.php
@@ -2,12 +2,9 @@
 				</div>
 			</div>
 			<?php wp_footer(); ?>
-<div style="display:none">
-Turkiyenin en sevilen <a title="porno" href="http://www.sexloving.com">porno</a> filmlerinin yer aldigi internet sitemiz olan ve <a title="sex resimleri" href="http://www.pornoizleyin.net/sex-resim/erotik/">sex resimleri</a> tarzi bir site olan <a title="porno video" href="http://www.hconi.com/">porno video</a>me sitemiz gercekten dillere destan bir durumda herkesin sevdigi bir site olarak tarihe gececege benziyor. Sitenin en belirgin ozelliklerinden birisi de Turkiye'de gercekten kaliteli ve muntazam, duzenli porno siteleri olmamasidir. Bu yuzden iste. Ayrica en net goruntu kalitesine sahip adresinde yayinlanmaktadir.
-</div>
 		</div>
 		<div id="push"></div>
 	</div>
-	<div id="footer"><p>Office of the City Clerk | City of Chicago | Susana A. Mendoza, City Clerk &copy; 2013</p></div>
+	<div id="footer"><p>Office of the City Clerk | City of Chicago | Susana A. Mendoza, City Clerk &copy; 2014</p></div>
 </body>
 </html>
